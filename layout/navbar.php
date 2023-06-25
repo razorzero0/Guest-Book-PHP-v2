5763f499069f4dbfd220fa39c3f4685454d668e7
Muh Ainun <87658206+razorzero0@users.noreply.github.com>
2023-06-25 07:55:38 +0700

    Update navbar.php

--- layout/navbar.php
+++ layout/navbar.php
@@ -8,7 +8,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <title>Admiministrator</title>
+  <title>Administrator</title>
 
   <!-- Bootstrap -->
   <link href="assets/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
